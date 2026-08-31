6151798215d9196d49e5fc1dde2601232e48ad8c
hlsfer <hlinsfer@gmail.com>
2026-08-30 22:34:36 -0300

    limitação de casas decimais para .2

--- atividades/atividade_1/codigo_3.py
+++ atividades/atividade_1/codigo_3.py
@@ -1,4 +1,4 @@
 #3. Elabore um programa que peça um número decimal e mostre a terça parte desse valor.
 num = float(input("Digite o número para mostrar a terça parte: "))
 terca_parte = num / 3
-print(f"A terça parte do número é: {terca_parte}")
+print(f"A terça parte do número é: {terca_parte:.2f}")

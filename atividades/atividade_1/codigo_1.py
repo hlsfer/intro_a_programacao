ac2009a2736e2c187044e66762c41e715c055274
hlsfer <hlinsfer@gmail.com>
2026-08-30 21:31:42 -0300

    primeiro commit

---
+++ atividades/atividade_1/codigo_1.py
@@ -0,0 +1,9 @@
+#1. Crie um programa que peça o nome de uma pessoa, o curso em que ela estuda e o período atual, e depois mostre todas essas 
+# informações em uma frase organizada.
+
+nome = input("Insira o seu nome: ")
+curso = input("Insira o seu curso: ")
+periodo_atual = input("Insira o seu periodo atual: ")
+print(f"Nome: {nome}")
+print(f"Curso selecionado: {curso}")
+print(f"Seu período atual é: {periodo_atual}")
